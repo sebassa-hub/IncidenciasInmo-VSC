f3771272d23f2f0c51aba2b3e28f67de3218ba1e
sebassa-hub <sebasnaruto32@gmail.com>
2025-10-09 20:55:55 -0500

    Fix/actualiza server index

--- server/index.js
+++ server/index.js
@@ -4,3 +4,6 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
