683596e3876d868ae6e7ea299435fbc6685a044e
sebassa-hub <sebasnaruto32@gmail.com>
2025-10-09 20:57:50 -0500

    commit de testeo server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,3 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
